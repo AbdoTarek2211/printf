f4130d18acbe9dd11551a7d8b90a68d4dcc5c78b
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-25 01:06:32 +0200

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,21 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 #include <unistd.h>
 /**
- * _printf - prints all strings
- * @format: the kind of format c for char s for string i for int f for
- * float.
- * Return: a string with tha arguments.
+ * _printf - Emulate the original.
+ *
+ * @format: Format by specifier.
+ *
+ * Return: count of chars.
  */
 int _printf(const char *format, ...)
 {
-va_list list;
-int i = 0, count = 0, char_count;
-va_start(list, format);
-
+int i = 0, count = 0, count_fun;
+va_list args;
+va_start(args, format);
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
 while (format[i])
 {
-char_count = 0;
+count_fun = 0;
 if (format[i] == '%')
 {
 if (!format[i + 1] || (format[i + 1] == ' ' && !format[i + 2]))
@@ -23,24 +26,21 @@ if (!format[i + 1] || (format[i + 1] == ' ' && !format[i + 2]))
 count = -1;
 break;
 }
-char_count += get_function(format[i + 1], list);
+count_fun += get_function(format[i + 1], args);
 if (count_fun == 0)
 count += _putchar(format[i + 1]);
-if (char_count == -1)
+if (count_fun == -1)
 count = -1;
 i++;
 }
 else
 {
-if (count == -1)
-_putchar(format[i]);
-else
-count += _putchar(format[i]);
+(count == -1) ? (_putchar(format[i])) : (count += _putchar(format[i]));
 }
 i++;
 if (count != -1)
-count += char_count;
+count += count_fun;
 }
-va_end(list);
+va_end(args);
 return (count);
 }
