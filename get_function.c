9e122576eeb2df42a2f63be3a8df9a8d40aa7c2a
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-25 01:03:57 +0200

    Update get_function.c

--- get_function.c
+++ get_function.c
@@ -1,38 +1,35 @@
-#include <stdlib.h>
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 /**
  * get_function - function specifiers.
- * @convention_spec: Conversion specifiers.
+ *
+ * @con_spec: Conversion specifiers.
  * @args: arguments.
  *
  * Return: char count.
  */
-int get_function(char convention_spec, va_list args)
+int get_function(char con_spec, va_list args)
 {
-int x = 0;
-int counter = 0;
-convention_s convs[] = {
-{'s', print_string},
-{'%', print_mod},
-{'d', print_digit},
-{'i', print_digit},
-{'r', print_rev_string},
-{0, NULL}
-};
-while (convs[x].str)
+int i = 0;
+int count_fun = 0;
+	specifiers_t spec[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_mod},
+		{'d', print_digit},
+		{'i', print_digit},
+		{'r', print_rev_string},
+		{0, NULL}
+	};
+while (spec[i].specifiers)
 {
-if (convs[x].str == convention_spec)
-{
-counter += convs[x].f(args);
-x++;
-}
+if (con_spec == spec[i].specifiers)
+count_fun += spec[i].f(args);
+i++;
 }
 if (count_fun == 0)
 {
-counter += _putchar('%');
-counter += _putchar(convention_spec);
+count_fun += _putchar('%');
+count_fun += _putchar(con_spec);
 }
-return (counter);
+return (count_fun);
 }
