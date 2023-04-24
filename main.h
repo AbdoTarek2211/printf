b9b65ce8d599892f2bab7d3a26f1eeda086be1bc
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-25 01:04:40 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,13 +1,18 @@
-#ifndef Main_H
-#define Main_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-#include <stdarg.h>
 #include <stdlib.h>
-typedef struct convention
+#include <stdarg.h>
+/**
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: The function associated
+ */
+typedef struct specifiers
 {
-char str;
+char specifiers;
 int (*f)(va_list);
-} convention_s;
+} specifiers_t;
 /*prototypes*/
 int _printf(const char *format, ...);
 int get_function(char s, va_list args);
